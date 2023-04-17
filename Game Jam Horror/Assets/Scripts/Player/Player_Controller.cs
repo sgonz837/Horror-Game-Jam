dac07398ad1808e3a18507ae53d156d2e0c96615
Kreigeru_an <129542828+Kriegerusan@users.noreply.github.com>
2023-04-17 11:42:30 +0200

    2nd day
    start iso mapping
    rotation for 3d object
    sorting layer is broken with 2d/3d

--- Game Jam Horror/Assets/Scripts/Player/Player_Controller.cs
+++ Game Jam Horror/Assets/Scripts/Player/Player_Controller.cs
@@ -7,13 +7,20 @@ public class Player_Controller : MonoBehaviour
 {
 
     [SerializeField] private float activeMoveSpeed = 2.5f;
+    [SerializeField] private GameObject threeDObject;
+
+    //for Testing purposes
+    private Camera camera;
+    private Vector2 mousePos;
 
     private Rigidbody2D playerRigidbody;
     private Vector2 moveDirection;
 
+
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
     }
 
     // Start is called before the first frame update
@@ -34,10 +41,24 @@ public class Player_Controller : MonoBehaviour
     {
         //in here to be frameRate independent
         MovePlayer();
+        RotatePlayer();
     }
 
     private void MovePlayer()
     {
         playerRigidbody.velocity = new Vector2(moveDirection.x, moveDirection.y).normalized * activeMoveSpeed;
+    }
+
+    private void RotatePlayer()
+    {
+        if(threeDObject != null)
+        {
+            //rotate the fbx on the z axis ?
+            mousePos = camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            threeDObject.transform.rotation = Quaternion.Euler(new Vector3(
+                -127,
+                0,
+                (-Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg)+90));
+        }
     }
 }
